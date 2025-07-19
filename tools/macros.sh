282c8ef3093a4767bd8c7f1d3f59638c70c28f74
kaDev0ps <153911387+kaDev0ps@users.noreply.github.com>
2025-07-19 13:05:12 +0300

    Create macros.sh

---
+++ tools/macros.sh
@@ -0,0 +1,3 @@
+# Install ZSH
+mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.install_zsh -O "$HOME/tools/.install_zsh" && chmod +x "$HOME/tools/.install_zsh" && "$HOME/tools/.install_zsh"
+mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.install_zsh -O "$HOME/tools/.install_zsh" && chmod +x "$HOME/tools/.install_zsh" && "$HOME/tools/.install_zsh"
