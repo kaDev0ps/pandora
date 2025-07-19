eefdc31c14e25db1586b60e22fae43fe92995347
kaDev0ps <153911387+kaDev0ps@users.noreply.github.com>
2025-07-19 13:12:19 +0300

    Update macros.sh

--- tools/macros.sh
+++ tools/macros.sh
@@ -1,3 +1,6 @@
 # Install ZSH
 mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.install_zsh -O "$HOME/tools/.install_zsh" && chmod +x "$HOME/tools/.install_zsh" && "$HOME/tools/.install_zsh"
-mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.install_zsh -O "$HOME/tools/.install_zsh" && chmod +x "$HOME/tools/.install_zsh" && "$HOME/tools/.install_zsh"
+# Add alias
+wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.bash_alias -O ~/.bash_aliases && echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" >> ~/.bashrc && echo "if [ -f ~/.bash_aliases ]; then source ~/.bash_aliases; fi" >> ~/.zshrc
+# Lazy journal
+wget https://github.com/Lifailon/lazyjournal/releases/download/0.7.9/lazyjournal-0.7.9-amd64.deb -O lazyjournal.deb && sudo dpkg -i lazyjournal.deb && sudo apt-get install -f
