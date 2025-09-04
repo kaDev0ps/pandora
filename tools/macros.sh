ac3947ba330b7ee63dfd1016d0c6f8bc165c8d7f
kaDev0ps <153911387+kaDev0ps@users.noreply.github.com>
2025-09-04 16:26:31 +0300

    Update macros.sh

--- tools/macros.sh
+++ tools/macros.sh
@@ -7,8 +7,7 @@ mkdir -p "$HOME/tools" && wget https://github.com/Lifailon/lazyjournal/releases/
 # Install nvim
 mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.nvim.sh -O "$HOME/tools/.install_nvim" && chmod +x "$HOME/tools/.nvim" && "$HOME/tools/.install_nvim"
 # Backup configs
-mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.backup_configs.sh -O "$HOME/tools/.backup_configs.sh" && chmod +x "$HOME/tools/.backup_configs.sh" && sudo cp "$HOME/tools/.backup_configs.sh" /usr/lib/ && sudo /usr/lib/.backup_configs.sh && ln -s /usr/lib/.backup_configs.sh $HOME/backup_configs.sh && ln -s /data/bakcup/services/ backups
-
+mkdir -p "$HOME/tools" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.backup_configs.sh -O "$HOME/tools/.backup_configs.sh" && chmod +x "$HOME/tools/.backup_configs.sh" && sudo cp "$HOME/tools/.backup_configs.sh" /usr/lib/ && sudo /usr/lib/.backup_configs.sh && ln -sf /usr/lib/.backup_configs.sh "$HOME/backup_configs.sh" || true && ln -sfn /data/backup/services/ "$HOME/backups"
 
 # Add alias
 wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.bash_alias -O ~/.bash_aliases && echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" >> ~/.bashrc && echo "if [ -f ~/.bash_aliases ]; then source ~/.bash_aliases; fi" >> ~/.zshrc
