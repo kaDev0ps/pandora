46735ecb25af6efa13c487d067c5d00dd8722936
kaDev0ps <153911387+kaDev0ps@users.noreply.github.com>
2025-11-05 11:27:12 +0300

    Update macros.sh

--- tools/macros.sh
+++ tools/macros.sh
@@ -6,6 +6,8 @@ mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandor
 mkdir -p "$HOME/tools" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.backup_configs.sh -O "$HOME/tools/.backup_configs.sh" && chmod +x "$HOME/tools/.backup_configs.sh" && sudo cp "$HOME/tools/.backup_configs.sh" /usr/lib/ && sudo /usr/lib/.backup_configs.sh && ln -sf /usr/lib/.backup_configs.sh "$HOME/backup_configs.sh" || true && ln -sfn /data/backup/services/ "$HOME/backups"
 # Ranger in ENV
 mkdir -p "$HOME/tools" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.env_ranger.sh -O "$HOME/tools/.env_ranger.sh" && chmod +x "$HOME/tools/.env_ranger.sh" && "$HOME/tools/.env_ranger.sh"
+# Add users
+mkdir -p "$HOME/tools" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/pssh/add_users.sh -O "$HOME/tools/pssh/add_users.sh" && chmod +x "$HOME/tools/pssh/add_users.sh" && "$HOME/tools/pssh/add_users.sh"
 # Add alias
 wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.bash_alias -O ~/.bash_aliases && echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" >> ~/.bashrc && echo "if [ -f ~/.bash_aliases ]; then source ~/.bash_aliases; fi" >> ~/.zshrc
 
