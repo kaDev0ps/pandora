61a0b0af57a92ee1d200006f692b1a74c2aae0f2
kaDev0ps <153911387+kaDev0ps@users.noreply.github.com>
2025-11-20 10:19:56 +0300

    Update add_users.sh

--- tools/pssh/add_users.sh
+++ tools/pssh/add_users.sh
@@ -13,24 +13,24 @@ is_alt_linux() {
 
 # User creds
 USER1=netrika_ka
-PASSWORD_HASH1='$6$XbcQtpB9Zo3SioF8$Ay.zAYsryFKcRO5PAHKn9V.yh8UoWoUowWCrr/ZEQhrFiYQrCDjfrR81vH9xJMcuPnVqMPPBku5nIl3uRNUAc.'
-SSH_KEY1='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIb8KYTeJGHIcZhBS3jnQRqXbnIF+2BMHz379myWyW4I a.karmash@n3med.ru'
+PASSWORD_HASH1='!!!!!!!!!!!!!!!!SHA256!!!!!!!!!!!!!'
+SSH_KEY1='!!!!!!!!!!!!!!!!!!!!!!SSH KEY!!!!!!!!!!!!'
 
-USER2=ansible
-PASSWORD_HASH2='$6$tvdfLIWJvMTAd7Is$ERcaC0cuf8h.23yPEEMOmT8FyLbVhGf3cAQb79Nx1k29EtpHpX2Z6sGa6as1AX2csO.lQ.lKyQtHE/f31x31R0'
-SSH_KEY2='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzVowOUyJqcubL0MylzupIy6Lr1bDy+rlSaNhmWRC+n ansible@szpv-rundeck-app-01'
+USER2=netrika_sd
+PASSWORD_HASH2='!!!!!!!!!!!!!!!!SHA256!!!!!!!!!!!!!'
+SSH_KEY2=!!!!!!!!!!!!!!!!!!!!!!SSH KEY!!!!!!!!!!!!'
 
 USER3=netrika_na
-PASSWORD_HASH3='$6$hbZIjkt5FXEMhDCM$WjKpfof8G/ZGMgh.JyqS2Q9yewsM4PdmOzfkIhWuLf.xRDjieDxIRARtwW3XkzVH0Cm5Hug2.Ej9C.Q/evTAQ/'
-SSH_KEY3='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9khHXvA3mk9icB+dwClC44HAloni84XHKvoL/XuKXj n.anisimov-20230206'
+PASSWORD_HASH3='!!!!!!!!!!!!!!!!SHA256!!!!!!!!!!!!!'
+SSH_KEY3='!!!!!!!!!!!!!!!!!!!!!!SSH KEY!!!!!!!!!!!!'
 
 USER4=netrika_ms
-PASSWORD_HASH4='$6$2OP5C0V.2MCMz1MO$NqU8593AlfcAsGhXu6sQR.3MOiz7qbkrDYv9jKs2SCM4I.eoGOi/mDcEMGCKC7F2HTMLpPig9rbCCH80ioyTM/'
-SSH_KEY4='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCTx70b7dbdrG+OiH0eLzoC53A3zHT9hJoizraIB3RaRNVMwBEv2NmwAfFWQgQH/j86xJ7lh+WPp/FopReFUQETwANfmB6Cbi1E7+h3jfLzgXIzgCStjKxuTyrimCT9Uvt7W9+a15+Qz4U+IwlgGMip8aoUtrZ5XSwsbzyNYiX0Eod1Bagb6tt6VF/ZSNINjyHPsHJUUzNZh3PwGcXQRBaJiQ7CU69BJ5vW2vOfr0Cvrqwf5eB9VChh3j05JkHAYpauZwpLz1Plg8pKTou5Il2Vj4zRwXgf3Ag5ImbT+jLatLH5umMhZ9M0fJgfff5EROkQTKxKKcvXIWEw6B/45vsxFWcIyWbrQfCbzsOYGSl3HWAO0jBjd2g4lJhOlgdE8QUnWz01S5+UltHF6Vg+KiI6xlStFfV7n3jdFtUsgBTpcW6toS4XCJVjJKPW1zfAgDL0zkRg+KQxe9gAM/Kl+OA3hyTVXS8piwQhFcBQLtcD7dqfJ6hHqdp/CbTYh6lcXjH+kli+lFAnqhU0gqRJ6ejiWnIVu0cFtwULqWErP+MDWY37saU5h4XJSnrkwm7U9DogONsZs8346pYNkz0iaAW6YSt4VUT5pdyt/H+ZQGnlwcVWlMI3cidyOAicbs45s+cbarwnVRL4qjcF2ruq6a6HPcRqyXrCC2dzdiOMGBfFkw== s.mironenko@n3med.ru'
+PASSWORD_HASH4='!!!!!!!!!!!!!!!!SHA256!!!!!!!!!!!!!'
+SSH_KEY4='!!!!!!!!!!!!!!!!!!!!!!SSH KEY!!!!!!!!!!!!'
 
 USER5=netrika_sr
-PASSWORD_HASH5='$6$fluvaCW6Jl.sQBW2$aURvL11oVV/9xlRNiITSxjpKB0ZlhxELSGRcvbLXoqY1gYAszb3cAh5jhhrvFjNbaTe3f5dGXbWhvdzRAt9P2/'
-SSH_KEY5='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC528lPgMSZwl3B2pSrIU2DNSz0Hzyte3fZQ8tDfTdOpQo5Qs/R2weWgoEHFTYJMMd60exoh35uPJ9kUqGWKCecLmce+q/xGNTi2GstP0jmxWxNyqWm+do9BNY1wyKbhXY4vUURJw3kk27Pu/FsAlM9ibeX+9fxCU9dXQ7Tqe5oKnkaiGSf43ti/sbA88n8/5HPDbW2ihQQxJkHKryGNeYP+WOZSimt13aHQ2kOG8LtZudLDgfpnKhRab1ilhlLdlHUa2kaZ54iH/SOOLdg+424SJozH/yPy2jMD4VUa/eqIXRoyjw1aXaEfdV3WzkFn5E0QfwRYsr22Xz+ZMBMWz2VJgal4yCuEBPYfYR2p3FlDxfQqdCgF/carE5JUaxtLcI10e9W0t08zMKW/BBAp1/cA/tvuVpsfkOpBZ2TND523eIRfwfuumJZA/bxGi19C0owzxqIjMHBKnLBP+MWKk9CvXnEzsbMxXhbNnEvKtP+4rWn1cWVAz0PPEw9L/M6/jc='
+PASSWORD_HASH5='!!!!!!!!!!!!!!!!SHA256!!!!!!!!!!!!!'
+SSH_KEY5='!!!!!!!!!!!!!!!!!!!!!!SSH KEY!!!!!!!!!!!!'
 
 # Ensure sudoers.d directory exists before processing users
 ensure_sudoers_d_dir() {
