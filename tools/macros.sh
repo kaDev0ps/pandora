5061affb12a7513a72a0c3b2f598a02cd42eb2b5
kaDev0ps <153911387+kaDev0ps@users.noreply.github.com>
2026-03-18 11:37:29 +0300

    Update macros.sh

--- tools/macros.sh
+++ tools/macros.sh
@@ -4,8 +4,6 @@ mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandor
 mkdir -p "$HOME/tools" && wget https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.packages.sh -O "$HOME/tools/.packages.sh" && chmod +x "$HOME/tools/.packages.sh" && "$HOME/tools/.packages.sh"
 # Backup configs
 mkdir -p "$HOME/tools" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.backup_configs.sh -O "$HOME/tools/.backup_configs.sh" && chmod +x "$HOME/tools/.backup_configs.sh" && sudo cp "$HOME/tools/.backup_configs.sh" /usr/lib/ && sudo /usr/lib/.backup_configs.sh && ln -sf /usr/lib/.backup_configs.sh "$HOME/backup_configs.sh" || true && ln -sfn /data/backup/services/ "$HOME/backups"
-# Ranger in ENV
-mkdir -p "$HOME/tools" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/.env_ranger.sh -O "$HOME/tools/.env_ranger.sh" && chmod +x "$HOME/tools/.env_ranger.sh" && "$HOME/tools/.env_ranger.sh"
 # Add users
 mkdir -p "$HOME/tools/pssh" && wget -q --show-progress https://raw.githubusercontent.com/kaDev0ps/pandora/main/tools/pssh/add_users.sh -O "$HOME/tools/pssh/add_users.sh" && chmod +x "$HOME/tools/pssh/add_users.sh" && "$HOME/tools/pssh/add_users.sh"
 # Welcome
